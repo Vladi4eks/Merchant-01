6f0204ff09484ddd4043555c4749f8d72d873af7
Vladi4ek <jeryygood@gmail.com>
2022-04-08 23:14:19 +0300

    Updated the application

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 /**
  * @author Kalashnikov Vladislav
  * @version 1.0.0
@@ -7,7 +9,58 @@ package app;
 
 public class App {
 
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static ProductA product;
+    static MerchantA merchantA;
+    static String infoMerchant;
+    static String infoProduct;
+
     public static void main(String[] args) {
-        System.out.println("This is my new application!");
+        initVars();
+        showData(processData());
+    }
+
+    private static void initVars() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        merchantName = scan.nextLine();
+
+        System.out.println("Введите ваш номер телефона: ");
+        phone = scan.nextLine();
+
+        System.out.println("Введите ваш email: ");
+        email = scan.nextLine();
+
+        System.out.println("Введите имя товара: ");
+        productName = scan.nextLine();
+
+        System.out.println("Введите кол-во товара: ");
+        quantity = scan.nextInt();
+
+        System.out.println("Введите стоимость товара: ");
+        price = scan.nextDouble();
+
+        scan.close();
+    }
+
+    private static String processData() {
+        merchantA = new MerchantA(merchantName, phone, email);
+        infoMerchant = merchantA.infoMerchant();
+        product = new ProductA(productName, quantity, price);
+        infoProduct = product.infoProduct();
+        double sales = product.calcSales(quantity, price);
+        roundBonus = Rounder.roundValue(merchantA.calcBonus(sales));
+        return infoMerchant + infoProduct + "\nБонус (грн.): " + roundBonus;
+    }
+
+    private static void showData(String output) {
+        System.out.println(output);
     }
 }
