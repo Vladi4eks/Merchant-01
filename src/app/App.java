b76d39875f00ac0b5a8fb166977331a641d0e963
Vladi4ek <jeryygood@gmail.com>
2022-04-08 21:16:14 +0300

    Initial commit

---
+++ src/app/App.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("This is my new application!");
+    }
+}
