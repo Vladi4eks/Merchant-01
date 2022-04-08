6137ff0e51d2383db1b6a0ad3ad73d666facd3bd
Vladi4ek <jeryygood@gmail.com>
2022-04-08 22:39:04 +0300

    Updated the application

--- src/app/App.java
+++ src/app/App.java
@@ -7,7 +7,32 @@ package app;
 
 public class App {
 
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+
     public static void main(String[] args) {
-        System.out.println("This is my new application!");
+        initVars();
+        showData(processData());
+    }
+
+    private static void initVars() {
+        merchantName = "Bob";
+        email = "bobs@mail.com";
+        phone = "059-123-4567";
+        productName = "Bayraktar";
+        quantity = 5000;
+        price = 50000;
+    }
+
+    private static String processData() {
+        return null;
+    }
+
+    private static void showData(String output) {
+        System.out.println(output);
     }
 }
