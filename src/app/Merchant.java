c6cecf3583d116cac67812955daa6896ed420476
Vladi4ek <jeryygood@gmail.com>
2022-04-08 21:41:31 +0300

    Added model for merchant

---
+++ src/app/Merchant.java
@@ -0,0 +1,25 @@
+package app;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class Merchant {
+
+    private final String name;
+    private final String phone;
+
+    public Merchant(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
