2ce6ee3a5b75ac276ffd6cf21f788632b94a6958
Vladi4ek <jeryygood@gmail.com>
2022-04-08 22:24:04 +0300

    Added model for product

---
+++ src/app/ProductA.java
@@ -0,0 +1,30 @@
+package app;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class ProductA extends Product implements Sales {
+
+    int quantity;
+    double price;
+
+    public ProductA(String name, int quantity, double price) {
+        super(name);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String infoProduct() {
+
+        String name = getName();
+        String resultSale = Rounder.roundValue(calcSales(quantity, price));
+        return "Товар: " + name + "\nОбъем продаж (грн.): " + resultSale;
+    }
+
+    @Override
+    public double calcSales(int quantity, double price) {
+        return quantity * price;
+    }
+}
