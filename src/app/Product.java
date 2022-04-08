9239bcff0228fb965da6473d403b9593eb268fd1
Vladi4ek <jeryygood@gmail.com>
2022-04-08 21:33:04 +0300

    Added model for product

---
+++ src/app/Product.java
@@ -0,0 +1,19 @@
+package app;
+
+/**
+ * @author Kalashnikov Vladislav
+ * @version 1.0.0
+ */
+
+public class Product {
+
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
